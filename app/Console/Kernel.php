371e8eec00812fc1d163755414c3b84b31cc245d
Johan le Roux <jleroux99@gmail.com>
2018-05-07 22:36:05 +0200

    Disable Scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('twitter:fetchFollowing')
-            ->everyMinute();
+        // $schedule->command('twitter:fetchFollowing')
+        //     ->everyMinute();
     }
 
     /**
